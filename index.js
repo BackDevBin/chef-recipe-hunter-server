9a34bcc4956ebcd0b50d9e7b60780acfa4385e66
BackDevBin <tishansarker873@gmail.com>
2023-05-02 12:51:55 +0600

    send chef data to server

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const chefs = require('./data/chefs.json');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+})
+
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = chefs.find(n => n._id === parseInt(id));
+    res.send(chef);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`listening on port ${port}`)
 })
